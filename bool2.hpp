a8b23b599b08679991ccdbd0e1b4993b773f90fc
fmutant <funnymutant@gmail.com>
2019-05-24 00:46:10 +0300

    semicolon->coma in store methods consts in ctors

--- bool2.hpp
+++ bool2.hpp
@@ -11,9 +11,9 @@ struct bool2 {
   HLML_INLINEF bool2() {}
   HLML_INLINEF explicit bool2(VI128 v) : m(v) {}
   HLML_INLINEF explicit bool2(b8 x, b8 y) : bool2(funcs::Ashiftl(funcs::setXYZW(x, y, consts::snZero, consts::snZero), 31u)) {}
-  HLML_INLINEF explicit bool2(b8* p) : bool2(p[0], p[1]) {}
+  HLML_INLINEF explicit bool2(const b8* p) : bool2(p[0], p[1]) {}
 
-  HLML_INLINEF void store(b8 *p) const { p[0] = x(); p[1] = y(); }
+  HLML_INLINEF void store(b8 *p) const { p[0] = x(), p[1] = y(); }
 
   HLML_INLINEF void setX(b8 x) { m = inserti(m, x << 31u, 0); }
   HLML_INLINEF void setY(b8 y) { m = inserti(m, y << 31u, 1); }
@@ -27,13 +27,13 @@ struct bool2 {
   HLML_INLINEF bool2 yy() const { return shuffleb2(*this, 1, 1); }
 };
 HLML_INLINEF bool2    operator!   (bool2 a) { a.m = funcs::notAandB(a.m, consts::vsignppnn); return a; }
+HLML_INLINEF bool2    operator~   (bool2 a) { return !a; }
 HLML_INLINEF bool2    operator&   (bool2 a, bool2 b) { a.m = funcs::AandB(a.m, b.m); return a; }
 HLML_INLINEF bool2    operator|   (bool2 a, bool2 b) { a.m = funcs::AorB(a.m, b.m); return a; }
 HLML_INLINEF bool2    operator^   (bool2 a, bool2 b) { a.m = funcs::AxorB(a.m, b.m); return a; }
 HLML_INLINEF bool2&   operator&=  (bool2& a, bool2 b) { a = a & b; return a; }
 HLML_INLINEF bool2&   operator|=  (bool2& a, bool2 b) { a = a | b; return a; }
 HLML_INLINEF bool2&   operator^=  (bool2& a, bool2 b) { a = a ^ b; return a; }
-HLML_INLINEF bool2    operator~   (bool2 a) { return !a; }
 HLML_INLINEF u32      mask        (bool2 v) { return bool2::flagsall & funcs::movemask(v.m); }
 HLML_INLINEF b8       all         (bool2 v) { return bool2::flagsall == mask(v); }
 HLML_INLINEF b8       any         (bool2 v) { return mask(v); }
--- bool3.hpp
+++ bool3.hpp
@@ -11,10 +11,10 @@ struct bool3 {
   HLML_INLINEF bool3() {}
   HLML_INLINEF explicit bool3(VI128 v) : m(v) {}
   HLML_INLINEF explicit bool3(b8 x, b8 y, b8 z) : m(funcs::Ashiftl(funcs::setXYZW(x, y, z, consts::snZero), 31u)) {}
-  HLML_INLINEF explicit bool3(b8* p) : bool3(p[0], p[1], p[2]) {}
+  HLML_INLINEF explicit bool3(const b8* p) : bool3(p[0], p[1], p[2]) {}
   HLML_INLINEF explicit bool3(bool2 v, b8 z) : bool3(v.x(), v.y(), z) {}
 
-  HLML_INLINEF void store(b8 *p) const { p[0] = x(); p[1] = y(); p[2] = z(); }
+  HLML_INLINEF void store(b8 *p) const { p[0] = x(), p[1] = y(), p[2] = z(); }
 
   HLML_INLINEF void setX(b8 x) { m = inserti(m, x << 31u, 0); }
   HLML_INLINEF void setY(b8 y) { m = inserti(m, y << 31u, 1); }
@@ -62,13 +62,13 @@ struct bool3 {
   HLML_INLINEF bool3 zzz() const { return shuffleb3(*this, 2, 2, 2); }
 };
 HLML_INLINEF bool3    operator!   (bool3 a) { a.m = funcs::notAandB(a.m, consts::vsignpppn); return a; }
+HLML_INLINEF bool3    operator~   (bool3 a) { return !a; }
 HLML_INLINEF bool3    operator&   (bool3 a, bool3 b) { a.m = funcs::AandB(a.m, b.m); return a; }
 HLML_INLINEF bool3    operator|   (bool3 a, bool3 b) { a.m = funcs::AorB(a.m, b.m); return a; }
 HLML_INLINEF bool3    operator^   (bool3 a, bool3 b) { a.m = funcs::AxorB(a.m, b.m); return a; }
 HLML_INLINEF bool3&   operator&=  (bool3& a, bool3 b) { a = a & b; return a; }
 HLML_INLINEF bool3&   operator|=  (bool3& a, bool3 b) { a = a | b; return a; }
 HLML_INLINEF bool3&   operator^=  (bool3& a, bool3 b) { a = a ^ b; return a; }
-HLML_INLINEF bool3    operator~   (bool3 a) { return !a; }
 HLML_INLINEF u32      mask        (bool3 v) { return bool3::flagsall & funcs::movemask(v.m); }
 HLML_INLINEF b8       all         (bool3 v) { return bool3::flagsall == mask(v); }
 HLML_INLINEF b8       any         (bool3 v) { return mask(v); }
